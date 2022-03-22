14b970e2b71f348daf4b15df314ca831d293866c
Mvergilio <mvergiliodesouza@gmail.com>
2022-03-22 11:05:28 -0300

    started the style of the page

--- Digital-Business-Card/src/components/Img-section.jsx
+++ Digital-Business-Card/src/components/Img-section.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 export default class Image extends Component {
   render() {
-    const style = {
-      width: "100px",
-    };
     return (
-      <div className="img--container" style={style}>
-        <img src="./src/images/profile-picture.jpg" width={"100%"} alt="" />
+      <div className="img--container">
+        <img src="./src/images/profile-picture.jpg" alt="" />
       </div>
     );
   }
