c9b9ebb8f4b205fa3ca155db95c5369ee38a4896
Mvergilio <mvergiliodesouza@gmail.com>
2022-03-25 11:06:00 -0300

    added the nav-bar component

---
+++ airbnb-clone/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav className="nav--container">
+      <img src="./src/images/image-logo.png" alt="" />
+    </nav>
+  );
+}
