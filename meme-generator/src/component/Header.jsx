d3ba4cd21200ed1b50b8c52be3f74aae657ab1f1
Mvergilio <mvergiliodesouza@gmail.com>
2022-04-02 10:55:12 -0300

    start the meme-generator project

---
+++ meme-generator/src/component/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header--title--container">
+        <img src="./src/images/troll-face.png" alt="" />
+        <h1>Meme Generator</h1>
+      </div>
+      <h2>React Project</h2>
+    </header>
+  );
+}
---
+++ meme-generator/src/component/Meme.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Meme() {
+  return (
+    <form action="get" className="form">
+      <div className="form--input-container">
+        <input type="text" placeholder="Top Text" name="first" id="first" />
+        <input
+          type="text"
+          placeholder="Bottom Text"
+          name="second"
+          id="second"
+        />
+      </div>
+      <button className="form--input--btn">Get a new image meme</button>
+    </form>
+  );
+}
